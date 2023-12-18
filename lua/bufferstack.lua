1d3ca8b529816df9df72cfade495df2417f8b13b
gremble0 <hermanstornes@gmail.com>
2023-12-18 04:15:35 +0100

    Add some docstrings and rename functions

--- lua/bufferstack.lua
+++ lua/bufferstack.lua
@@ -2,6 +2,10 @@
 ---@field buffers integer[]
 local M = {}
 
+---Shifts all elements in the given list one step to the left,
+---leftmost element goes to the back
+---@param list any[]
+---@return any[]
 local function shift_left(list)
   if #list <= 1 then
     return list
@@ -9,7 +13,7 @@ local function shift_left(list)
 
   local first = list[1]
 
-  for i = 1, #list - 1 do
+  for i = 1, #list -1 do
       list[i] = list[i + 1]
   end
 
@@ -18,6 +22,10 @@ local function shift_left(list)
   return list
 end
 
+---Shifts all elements in the given list one step to the right,
+---rightmost element goes to the front
+---@param list any[]
+---@return any[]
 local function shift_right(list)
   if #list <= 1 then
     return list
@@ -25,7 +33,7 @@ local function shift_right(list)
 
   local last = list[#list]
 
-  for i = #list, 2, - 1 do
+  for i = #list, 2, -1 do
     list[i] = list[i - 1]
   end
 
@@ -34,8 +42,9 @@ local function shift_right(list)
   return list
 end
 
+---Adds the buffer to the front of the internal stack of open buffers
 ---@param buffer integer
-function M.add_buffer(buffer)
+function M.push_front(buffer)
   local new_buffers = { buffer }
 
   for i, buf in ipairs(M.buffers) do
@@ -47,12 +56,16 @@ function M.add_buffer(buffer)
   M.buffers = new_buffers
 end
 
+---Updates the internal stack of buffers by shifting it to the right
+---and sets the current buffer to the new element at the front
 function M.bnext()
   local buffers = shift_right(M.buffers)
   vim.api.nvim_set_current_buf(buffers[1])
   M.buffers = buffers
 end
 
+---Updates the internal stack of buffers by shifting it to the left
+---and sets the current buffer to the new element at the front
 function M.bprevious()
   local buffers = shift_left(M.buffers)
   vim.api.nvim_set_current_buf(buffers[1])
@@ -65,7 +78,7 @@ function M.setup()
   local buffer_stack_group = vim.api.nvim_create_augroup("BufferStack", {})
   vim.api.nvim_create_autocmd("BufWinEnter", {
     group = buffer_stack_group,
-    callback = function() M.add_buffer(vim.api.nvim_get_current_buf()) end
+    callback = function() M.push_front(vim.api.nvim_get_current_buf()) end
   })
 end
 
